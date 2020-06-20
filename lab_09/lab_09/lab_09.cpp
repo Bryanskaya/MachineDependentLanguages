9e54cc3db83efca4993f2bc377ced60054d736a8
Bryanskaya <bryanskayakatya@yandex.ru>
2020-06-20 16:37:39 +0300

    ADD: lab 9

---
+++ lab_09/lab_09/lab_09.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, num;
+    double temp, sum = 0;
+
+    printf("Input amount of numbers: ");
+    scanf_s("%d", &n);
+
+    if (n <= 0)
+    {
+        printf("\nERROR\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("\nInput numbers: \n");
+
+    num = n;
+    while (n > 0)
+    {
+        scanf_s("%lf", &temp);
+        sum += temp;
+        n -= 1;
+    }
+
+    printf("Average: %lf\n\n", sum / num);
+    
+    return EXIT_SUCCESS;
+}
+
